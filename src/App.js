5d672e785b5962bb01cb868eadc02c7d94857d68
Slavich <ss_slavich@mail.ru>
2022-04-10 22:15:19 +0300

    Starting create elements. Before onDrags...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const initCards = [
+  { id: 1, order: 1, text: "1" },
+  { id: 2, order: 2, text: "2" },
+  { id: 3, order: 3, text: "3" },
+  { id: 4, order: 4, text: "4" },
+];
+
+const onDragStart = (e, card) => {
+  console.log('onDragStart')
+}
+
+const onDragEnd = (e, card) => {
+  console.log('onDragEnd')
+}
+
+const onDragLeave = (e, card) => {
+  console.log('onDragLeave')
+}
+
+const onDrop = (e, card) => {
+  console.log('onDrop')
+}
+
+const onDragOver = (e, card) => {
+  console.log('onDragOver')
+}
 
 function App() {
+  const [cards, setCards] = useState(initCards);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cardContainer">
+        {cards.map((c) => {
+          return <div key={c.id} className="cardItem" draggable="true" 
+          onDragStart={onDragStart}
+          onDragEnd={onDragEnd}
+          onDragOver={onDragOver}
+          onDragLeave={onDragLeave}
+          onDrop={onDrop}
+          >{c.text}</div>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,5 @@
+const Card = (props) => {
+    return <div>
+        
+    </div>
+}
