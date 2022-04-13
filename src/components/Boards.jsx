234484510c09b178bfa4439a6b001b2c279a51e7
32 кабинет <ss_slavich@mail.ru>
2022-04-13 17:27:36 +0300

    Ceate boards items

--- src/components/Boards.jsx
+++ src/components/Boards.jsx
@@ -1,48 +1,60 @@
 import { useState } from "react";
 import "./Boards.css";
 
-const initBoard1 = [
-  { id: 11, text: "1-1" },
-  { id: 12, text: "1-2" },
-  { id: 13, text: "1-3" },
-];
+const initBoard = {
+  board1: {
+    id: 1,
+    items: [
+      { id: 11, text: "1-1" },
+      { id: 12, text: "1-2" },
+      { id: 13, text: "1-3" }],
+  },
 
-const initBoard2 = [
-  { id: 21, text: "2-1" },
-  { id: 22, text: "2-2" },
-  { id: 23, text: "2-3" },
-];
+  board2: {
+    id: 2,
+    items: [
+      { id: 21, text: "2-1" },
+      { id: 22, text: "2-2" },
+      { id: 23, text: "2-3" }
+    ],
+  },
 
-const initBoard3 = [
-  { id: 31, text: "3-1" },
-  { id: 32, text: "3-2" },
-  { id: 33, text: "3-3" },
-];
+
+  board3: {
+    id: 3,
+    items: [
+      { id: 31, text: "3-1" },
+      { id: 32, text: "3-2" },
+      { id: 33, text: "3-3" }
+    ]
+  }
+}
+
 
 const Boards = () => {
-  const [board1, setBoard1] = useState(initBoard1);
-  const [board2, setBoard2] = useState(initBoard2);
-  const [board3, setBoard3] = useState(initBoard3);
+  const [boards, setBoards] = useState(initBoard);
+  console.log(boards)
+
   return (
     <div>
+
       <div className="boardContainer">
-        <div className="board">
-          {board1.map((c) => {
-            return <div className="item">{c.text}</div>;
-          })}
-        </div>
+        {Object.entries(boards).map((board) => {
+          return (
+            <div className="boardItem">
 
-        <div className="board">
-          {board2.map((c) => {
-            return <div className="item">{c.text}</div>;
-          })}
-        </div>
+              <div>{board[0]}</div>
+              {board[1].items.map(item => {
+                return (
+                  <div className="item">
+                    {item.text}
+                  </div>)
+              }
+              )}
 
-        <div className="board">
-          {board1.map((c) => {
-            return <div className="item">{c.text}</div>;
-          })}
-        </div>
+            </div>)
+        })}
+
       </div>
     </div>
   );
